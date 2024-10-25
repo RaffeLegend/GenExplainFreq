c670c02ea23db8b7d28ab5c949d761a1a67a0dea
RaffeLegend <oldgirafferiver@gmail.com>
2024-10-26 00:59:33 +0800

    [#1] func: add support function

--- utils/utils.py
+++ utils/utils.py
@@ -107,3 +107,21 @@ def pad_img_to_square(img: np.ndarray):
         img = np.pad(img, ((0, new_size - H), (0, new_size - W), (0, 0)), mode="constant")
         assert img.shape[0] == img.shape[1] == new_size
     return img
+
+def mkdirs(paths):
+    if isinstance(paths, list) and not isinstance(paths, str):
+        for path in paths:
+            mkdir(path)
+    else:
+        mkdir(paths)
+
+
+def mkdir(path):
+    if not os.path.exists(path):
+        os.makedirs(path)
+
+
+def unnormalize(tens, mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]):
+    # assume tensor of shape NxCxHxW
+    return tens * torch.Tensor(std)[None, :, None, None] + torch.Tensor(
+        mean)[None, :, None, None]
